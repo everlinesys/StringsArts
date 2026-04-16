42df84d603999fb09615312e32e24aae58600c64
Azhar <azhar.subair.2@gmail.com>
2026-04-17 02:02:53 +0530

    tenant code

--- src/student/layout/StudentHeader.jsx
+++ src/student/layout/StudentHeader.jsx
@@ -6,7 +6,7 @@ import { useBranding } from "../../shared/hooks/useBranding";
 export default function StudentHeader({ onMenuClick }) {
     const navigate = useNavigate();
     const user = getUser();
-    const brand= useBranding();
+    const brand = useBranding();
 
     return (
         <header className="h-16 bg-white border-b flex items-center justify-between px-6 shadow-sm">
@@ -18,13 +18,13 @@ export default function StudentHeader({ onMenuClick }) {
                 <button
                     onClick={onMenuClick}
                     className="lg:hidden text-gray-700 "
-                    style={{background:"transparent", color: "gray", padding:"0px"}}
+                    style={{ background: "transparent", color: "gray", padding: "0px" }}
                 >
                     <MdMenu size={24} />
                 </button>
 
-                <h1 className="text-md lg:text-lg font-semibold text-gray-800" style={{fontSize:"medium" , fontWeight:"bold"}}>
-                    {brand.siteName}
+                <h1 className="text-md lg:text-lg font-semibold text-gray-800" style={{ fontSize: "medium", fontWeight: "bold" }}>
+                    {brand.siteName?.split(' ')[0].toUpperCase()}
                 </h1>
             </div>
 
